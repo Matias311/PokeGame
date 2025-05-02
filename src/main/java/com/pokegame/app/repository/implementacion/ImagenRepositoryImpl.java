cc08569f56f56cddf5d45b9b6ec2da22868073f1
Matias Arias <matiasarias3103@gmail.com>
2025-05-02 16:26:52 -0400

    [FIX] optimizacion del codigo pokedex gui, eliminacion de metodos innecesario que no se han utilizado o no seran ocupado

--- src/main/java/com/pokegame/app/repository/implementacion/ImagenRepositoryImpl.java
+++ src/main/java/com/pokegame/app/repository/implementacion/ImagenRepositoryImpl.java
@@ -14,45 +14,12 @@ public class ImagenRepositoryImpl implements ImagenRepository<Imagen> {
   @Override
   public Imagen buscarImagenPorIdPortadaPokemon(int id) {
     Imagen imagen = null;
-    try {
-      PreparedStatement state =
-          conn.prepareStatement("SELECT imagen_frente FROM Imagenes WHERE id_pokemon = ?");
+    try (PreparedStatement state =
+        conn.prepareStatement("SELECT imagen_frente FROM Imagenes WHERE id_pokemon = ?"); ) {
       state.setInt(1, id);
       ResultSet result = state.executeQuery();
       while (result.next()) {
         imagen = new Imagen(result.getString("imagen_frente"));
-      }
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    return imagen;
-  }
-
-  @Override
-  public Imagen buscarImagenPorIdPokemon(int id) {
-    Imagen imagen = null;
-    try {
-      PreparedStatement state =
-          conn.prepareStatement("SELECT * FROM Imagenes WHERE id_pokemon = ?");
-      state.setInt(1, id);
-      ResultSet result = state.executeQuery();
-      imagen = crearImagen(result);
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    return imagen;
-  }
-
-  public Imagen crearImagen(ResultSet result) {
-    Imagen imagen = null;
-    try {
-      while (result.next()) {
-        imagen =
-            new Imagen(
-                result.getInt("id"),
-                result.getString("imagen_frente"),
-                result.getString("imagen_espalda"),
-                result.getInt("id_pokemon"));
       }
     } catch (Exception e) {
       System.out.println(e);
