b3fda637765b81958e18f3028b13d9efd41b3c68
Matias Arias <matiasarias3103@gmail.com>
2025-05-02 16:26:52 -0400

    [FIX] optimizacion del codigo pokedex gui, eliminacion de metodos innecesario que no se han utilizado o no seran ocupado

--- src/main/java/com/pokegame/app/repository/ImagenRepository.java
+++ src/main/java/com/pokegame/app/repository/ImagenRepository.java
@@ -3,7 +3,5 @@ package com.pokegame.app.repository;
 /** ImagenRepository. */
 public interface ImagenRepository<T> {
 
-  T buscarImagenPorIdPokemon(int id);
-
   T buscarImagenPorIdPortadaPokemon(int id);
 }
--- src/main/java/com/pokegame/app/repository/implementacion/PokemonRepositoryImpl.java
+++ src/main/java/com/pokegame/app/repository/implementacion/PokemonRepositoryImpl.java
@@ -5,7 +5,6 @@ import com.pokegame.app.repository.PokemonRepository;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +16,10 @@ public class PokemonRepositoryImpl implements PokemonRepository<Pokemon> {
   @Override
   public List<Pokemon> traerPaginacionPokemon(int inicio, int cant) {
     List<Pokemon> lista = new ArrayList<Pokemon>();
-    try {
-      PreparedStatement state =
-          conn.prepareStatement(
-              "SELECT id, nombre FROM Pokemon ORDER BY id ASC OFFSET ? ROWS FETCH NEXT ? ROWS"
-                  + " ONLY");
+    try (PreparedStatement state =
+        conn.prepareStatement(
+            "SELECT id, nombre FROM Pokemon ORDER BY id ASC OFFSET ? ROWS FETCH NEXT ? ROWS"
+                + " ONLY"); ) {
       state.setInt(1, inicio);
       state.setInt(2, cant);
       ResultSet result = state.executeQuery();
@@ -30,20 +28,6 @@ public class PokemonRepositoryImpl implements PokemonRepository<Pokemon> {
       System.out.println(e);
     }
     return lista;
-  }
-
-  @Override
-  public List<Pokemon> traerTodosLosPokemon() {
-    List<Pokemon> listaPokemon = new ArrayList<Pokemon>();
-    try {
-      Statement state = conn.createStatement();
-      String query = "SELECT * FROM Pokemon";
-      ResultSet myRes = state.executeQuery(query);
-      listaPokemon = crearListaPokemon(myRes);
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    return listaPokemon;
   }
 
   @Override
@@ -74,35 +58,6 @@ public class PokemonRepositoryImpl implements PokemonRepository<Pokemon> {
   public Pokemon traerPokemonId(Pokemon id) {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'traerPokemonId'");
-  }
-
-  /**
-   * Crea un pokemon obteniendo toda la informacion de la base de datos.
-   *
-   * @param resultado ResultSet
-   * @return List
-   */
-  public List<Pokemon> crearListaPokemon(ResultSet resultado) {
-    List<Pokemon> lista = new ArrayList<Pokemon>();
-    try {
-      while (resultado.next()) {
-        Pokemon pokemon =
-            new Pokemon(
-                resultado.getInt("id"),
-                resultado.getString("nombre"),
-                resultado.getString("descripcion"),
-                resultado.getInt("altura"),
-                resultado.getInt("peso"),
-                resultado.getString("region"),
-                resultado.getInt("vida"),
-                resultado.getInt("ataque"),
-                resultado.getInt("defensa"));
-        lista.add(pokemon);
-      }
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    return lista;
   }
 
   /**
