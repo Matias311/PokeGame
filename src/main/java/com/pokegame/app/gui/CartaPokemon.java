612ab5a98fbcefa8d7870fd3d0dc4d9170958b31
Matias Arias <matiasarias3103@gmail.com>
2025-05-04 15:40:35 -0400

    [FEATURE] agregado de sombreas a las cartas, y modificacion de estilo del cursor

--- src/main/java/com/pokegame/app/gui/CartaPokemon.java
+++ src/main/java/com/pokegame/app/gui/CartaPokemon.java
@@ -4,14 +4,25 @@ import com.pokegame.app.modelo.Imagen;
 import com.pokegame.app.modelo.Pokemon;
 import com.pokegame.app.util.ImagenCache;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 /** CartaPokemon. */
 public class CartaPokemon extends JPanel {
+
+  // TODO: Crear los apuntes de los thread
+  // TODO: Crear estilo de las cartas, sombre y borde para destingir cada carta
 
   /**
    * Obtiene el objeto pokemon, imagen y crea la carta para el menu principal.
@@ -22,8 +33,10 @@ public class CartaPokemon extends JPanel {
   public CartaPokemon(Pokemon pokemon, Imagen imagen) {
     setPreferredSize(new Dimension(150, 150));
     setLayout(new BorderLayout());
+    setOpaque(false);
     // Nombre del pokemon
     JLabel nombrePokemon = new JLabel(pokemon.getNombre(), SwingConstants.CENTER);
+    nombrePokemon.setBorder(BorderFactory.createEmptyBorder(0, 0, 7, 0));
     nombrePokemon.setFont(new Font("Arial", Font.BOLD, 16));
     add(BorderLayout.SOUTH, nombrePokemon);
     // imagen
@@ -43,5 +56,32 @@ public class CartaPokemon extends JPanel {
     } catch (Exception e) {
       System.out.println(e);
     }
+    addMouseListener(
+        new MouseAdapter() {
+          @Override
+          public void mouseEntered(MouseEvent e) {
+            setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+          }
+
+          @Override
+          public void mouseExited(MouseEvent e) {
+            setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+          }
+        });
+  }
+
+  // Creacion sombra carta del pokemon
+  @Override
+  protected void paintComponent(Graphics g) {
+    int shadowSize = 2;
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g2.setColor(new Color(231, 232, 227));
+    g2.fillRoundRect(
+        shadowSize, shadowSize, getWidth() - shadowSize, getHeight() - shadowSize, 20, 20);
+    g2.setColor(getBackground());
+    g2.fillRoundRect(0, 0, getWidth() - shadowSize, getHeight() - shadowSize, 20, 20);
+    g2.dispose();
+    super.paintComponent(g);
   }
 }
